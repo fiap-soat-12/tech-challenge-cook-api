1bffe1a703ece79f012d2b3d43f1e04f93473484
DiguyaDeveloper <diegocecconcwb@outlook.com>
2025-01-23 01:06:56 -0300

    feat(MESSAGE): :construction: Added feature to receive and send sqs messages
    Add product created success listener
    Add product to create publisher

---
+++ src/infrastructure/entrypoint/publishers/product-to-create.publisher.ts
@@ -0,0 +1,27 @@
+import { SendProductDto } from '@application/dto/send-product.dto';
+import { Logger } from '@application/interfaces/logger.interface';
+import { MessagePublisher } from '@application/interfaces/message-publisher.interface';
+import { SqsClient } from '@infrastructure/config/sqs.config';
+
+export class ProductToCreatePublisher
+  implements MessagePublisher<SendProductDto>
+{
+  private readonly queueUrl: string;
+
+  constructor(
+    private readonly sqsClient: SqsClient,
+    private readonly logger: Logger,
+  ) {
+    this.queueUrl = process.env.PRODUCT_CREATED_QUEUE_URL || '';
+    if (!this.queueUrl) {
+      throw new Error('Queue URL for Product Created not configured');
+    }
+  }
+
+  async publish(product: SendProductDto): Promise<void> {
+    await this.sqsClient.sendMessage(this.queueUrl, product);
+    this.logger.log(
+      `Product created event published: ${JSON.stringify(product)}`,
+    );
+  }
+}
