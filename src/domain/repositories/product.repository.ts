d5bef6dde2b84be6334dbefd0c3d880a227b9da9
DiguyaDeveloper <diegocecconcwb@outlook.com>
2025-01-25 13:42:50 -0300

    feat(PERSISTENCE): :construction: Added return in product update and delete

--- src/domain/repositories/product.repository.ts
+++ src/domain/repositories/product.repository.ts
@@ -2,8 +2,8 @@ import { Product } from '@domain/entities/product';
 import { PageCollection } from '@domain/models/page-collection';
 
 export interface ProductRepository {
-  create(product: Product): Promise<void>;
-  delete(id: string): Promise<void>;
+  create(product: Product): Promise<Product>;
+  delete(id: string): Promise<Product>;
   update(product: Product): Promise<Product>;
   findById(id: string): Promise<Product>;
   findAll(): Promise<Product[]>;
