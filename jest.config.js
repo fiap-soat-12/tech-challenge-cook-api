171e64e1d23e9bbc23318bd5edc48f127182b6c5
DiguyaDeveloper <diegocecconcwb@outlook.com>
2025-01-25 20:27:07 -0300

    feat(SONAR): :construction: Ajust sonar configuration

--- jest.config.js
+++ jest.config.js
@@ -1,26 +1,30 @@
 module.exports = {
   collectCoverage: true,
-  coverageDirectory: 'coverage',
-  coverageReporters: ['lcov', 'text', 'text-summary', 'html', 'json-summary'],
+  coverageDirectory: './coverage',
+  coverageReporters: ['lcov', 'json-summary'],
   testResultsProcessor: 'jest-sonar-reporter',
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: [
+    'src/**/*.{ts,js}',
+    '!src/**/*.module.{ts,js}',
+    '!src/main.{ts,js}',
+  ],
   reporters: [
     'default',
     [
       'jest-html-reporter',
       {
-        pageTitle: 'Test Report',
-        outputPath: './reports/test-report.html',
+        pageTitle: 'Tech Challenge cook api Test Report',
+        outputPath: 'coverage/jest-test-report.html',
         includeFailureMsg: true,
         includeConsoleLog: true,
       },
     ],
   ],
-  moduleFileExtensions: ['js', 'json', 'ts'],
-  rootDir: 'src',
-  testRegex: '.*\\.spec\\.ts$',
-  transform: {
-    '^.+\\.(t|j)s$': 'ts-jest',
-  },
-  collectCoverageFrom: ['**/*.(t|j)s'],
   testEnvironment: 'node',
 };
