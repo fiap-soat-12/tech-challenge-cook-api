a0b9132086fdc90ef06af224b7e0bb1088ce70da
DiguyaDeveloper <diegocecconcwb@outlook.com>
2025-02-02 00:08:12 -0300

    feat(PRODUCTS): :sparkles: Updates error message when product not found

--- src/domain/exceptions/product-not-found.exception.ts
+++ src/domain/exceptions/product-not-found.exception.ts
@@ -2,7 +2,7 @@ import { NotFoundDomainException } from './not-found.exception';
 
 export class ProductNotFoundException extends NotFoundDomainException {
   constructor(productId: string) {
-    super(`Product with id ${productId} not found`);
+    super(`Product Doesn't Exist with id: ${productId}`);
     this.name = 'ProductNotFoundException';
   }
 }
