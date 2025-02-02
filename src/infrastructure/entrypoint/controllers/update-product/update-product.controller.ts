07cab88e56efc954b11280e038b364cc8f9fb33a
DiguyaDeveloper <diegocecconcwb@outlook.com>
2025-02-01 23:29:07 -0300

    feat(VALIDATORS): :sparkles: Add UUID validator in request

--- src/infrastructure/entrypoint/controllers/update-product/update-product.controller.ts
+++ src/infrastructure/entrypoint/controllers/update-product/update-product.controller.ts
@@ -1,11 +1,14 @@
-import { UpdateProductDto } from '@application/dto/update-product.dto';
+import { UUID } from '@application/types/UUID.type';
 import { UpdateProductUseCase } from '@application/usecases/products/update-product/update-product.usecase';
 import { ProductNotFoundException } from '@domain/exceptions/product-not-found.exception';
+import { UpdateProductRequest } from '@infrastructure/entrypoint/request/update-product.request';
 import { GetProductResponse } from '@infrastructure/entrypoint/response/get-product.response';
+import { UUIDValidationPipe } from '@infrastructure/entrypoint/validators/null-or-valid-uuid/null-or-valid-uuid.validator';
 import { Body, Controller, HttpException, Param, Put } from '@nestjs/common';
 import {
   ApiBearerAuth,
   ApiOperation,
+  ApiParam,
   ApiResponse,
   ApiTags,
 } from '@nestjs/swagger';
@@ -19,10 +22,16 @@ export class UpdateProductController {
   @ApiOperation({ summary: 'Update a Product By ID' })
   @ApiResponse({ status: 200, description: 'Product updated successfully.' })
   @ApiResponse({ status: 404, description: 'Product not found.' })
+  @ApiParam({
+    name: 'id',
+    description: 'UUID of the product to update',
+    example: 'a6b9e046-fb5a-4a79-9d86-363e6fd20e11',
+    type: 'string',
+  })
   @Put(':id')
   async update(
-    @Param('id') id: string,
-    @Body() dto: UpdateProductDto,
+    @Param('id', UUIDValidationPipe) id: UUID,
+    @Body() dto: UpdateProductRequest,
   ): Promise<GetProductResponse> {
     try {
       const updatedProduct = await this.updateProductUseCase.execute(id, dto);
