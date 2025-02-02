dee5d54f098803ea4bed1b96543cf44b9333e462
DiguyaDeveloper <diegocecconcwb@outlook.com>
2025-02-02 00:48:00 -0300

    ci(test): :wrench: Update jest config file to add paths

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  moduleNameMapper: {
+    '^@application/(.*)$': '<rootDir>/src/application/$1',
+    '^@domain/(.*)$': '<rootDir>/src/domain/$1',
+    '^@infra/(.*)$': '<rootDir>/src/infra/$1',
+  },
   collectCoverage: true,
   coverageDirectory: './coverage',
   coverageReporters: ['lcov', 'json-summary'],
