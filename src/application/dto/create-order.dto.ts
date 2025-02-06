e23041cb85ae9269e8567ac83ae7109c98ec222c
DiguyaDeveloper <diegocecconcwb@outlook.com>
2025-02-06 02:21:41 -0300

    feat(ORDER): :sparkles: Added id in CreateOrderDto

--- src/application/dto/create-order.dto.ts
+++ src/application/dto/create-order.dto.ts
@@ -1,7 +1,9 @@
 import { OrderStatusEnum } from '@application/enums/order-status.enum';
+import { UUID } from '@application/types/UUID.type';
 
 export class CreateOrderDto {
   constructor(
+    public readonly id: UUID,
     public readonly sequence: number,
     public readonly status: OrderStatusEnum,
     public readonly products: { id: string; customization: string }[],
